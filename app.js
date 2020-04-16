342fcacedee84a9ce03230d43572e3c924a950a4
Luke Kruyer <kruyerl@gmail.com>
2020-04-15 21:47:28 -0400

    Parse Request Bodies

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-        console.log(req.url, req.method, req.headers)
-        // process.exit()
+        const { url, method } = req
+        console.log(url)
+        if (url === '/') {
+                res.setHeader('Content-Type', 'text/html')
+                res.write('<html>')
+                res.write('<head><title>NodeJSCourse</title></head>')
+                res.write(
+                        '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>'
+                )
+                res.write('</html>')
+                return res.end()
+        }
+
+        if (url === '/message' && method === 'POST') {
+                const body = []
+                req.on('data', chunk => {
+                        body.push(chunk)
+                })
+                req.on('end', () => {
+                        const parsedBody = Buffer.concat(body).toString()
+                        const message = parsedBody.split('=')[1]
+                        fs.writeFileSync('message.txt', message)
+                })
+                res.statusCode = 302
+                res.setHeader('Location', '/')
+                return res.end()
+        }
         res.setHeader('Content-Type', 'text/html')
         res.write('<html>')
         res.write('<head><title>NodeJSCourse</title></head>')
