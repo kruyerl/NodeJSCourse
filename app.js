4d675eb29a99f5a0fd821f4c2875896806440185
Luke Kruyer <kruyerl@gmail.com>
2020-04-15 21:16:33 -0400

    Parsing requests and sending responses

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-const http = require('http');
+const http = require('http')
 
 const server = http.createServer((req, res) => {
-        // console.log(req);
-});
+        console.log(req.url, req.method, req.headers)
+        // process.exit()
+        res.setHeader('Content-Type', 'text/html')
+        res.write('<html>')
+        res.write('<head><title>NodeJSCourse</title></head>')
+        res.write('<body><h1>Hello World!</h1></body>')
+        res.write('</html')
+        res.end()
+})
 
-server.listen(3000);
+server.listen(1234)
