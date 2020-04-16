2b0c66459557bfb4a829c7fefd7ee3feeeeffc73
Luke Kruyer <kruyerl@gmail.com>
2020-04-15 22:30:26 -0400

    Split routes to seperate file

--- app.js
+++ app.js
@@ -1,41 +1,7 @@
 const http = require('http')
-const fs = require('fs')
 
-const server = http.createServer((req, res) => {
-    const { url, method } = req
-    console.log(url)
-    if (url === '/') {
-        res.setHeader('Content-Type', 'text/html')
-        res.write('<html>')
-        res.write('<head><title>NodeJSCourse</title></head>')
-        res.write(
-            '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>'
-        )
-        res.write('</html>')
-        return res.end()
-    }
+const routes = require('./routes')
 
-    if (url === '/message' && method === 'POST') {
-        const body = []
-        req.on('data', chunk => {
-            body.push(chunk)
-        })
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString()
-            const message = parsedBody.split('=')[1]
-            fs.writeFile('message.txt', message, err => {
-                res.statusCode = 302
-                res.setHeader('Location', '/')
-                return res.end()
-            })
-        })
-    }
-    res.setHeader('Content-Type', 'text/html')
-    res.write('<html>')
-    res.write('<head><title>NodeJSCourse</title></head>')
-    res.write('<body><h1>Hello World!</h1></body>')
-    res.write('</html')
-    res.end()
-})
+const server = http.createServer(routes)
 
 server.listen(1234)
